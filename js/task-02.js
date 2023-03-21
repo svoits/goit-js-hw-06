0c1563c089572cf5f33941cf715e42b438bbe792
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-03-21 19:06:30 +0100

    solved

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+// select container for ingredients
+const container = document.querySelector('#ingredients');
+
+// iterate each ingredient and create <li class="item">${ingredient}</li>
+const ingredientItems = ingredients.map(ingredient => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add('item');
+
+  return ingredientItem;
+});
+
+// add all ingredients in the list
+container.append(...ingredientItems);
+
+// check result in the console
+console.log(container);
