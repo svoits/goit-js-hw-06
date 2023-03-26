4cc1240314f9d57ce1d706f45d05d7fdeb67f1ff
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-03-26 14:35:37 +0200

    solved task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// select span and button
+const refs = {
+  backgroundColorValue: document.querySelector('.color'),
+  changeColorButton: document.querySelector('.change-color'),
+};
+
+// add listener to the button
+refs.changeColorButton.addEventListener('click', onChangeColorButtonClick);
+
+// function that changes span text content and body bg color
+function onChangeColorButtonClick() {
+  const randomHexColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomHexColor;
+  refs.backgroundColorValue.textContent = randomHexColor;
+}
