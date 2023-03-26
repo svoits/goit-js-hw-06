b860fbb86a351f7b46dc1e738a77cb45bd74ff35
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-03-26 14:18:51 +0200

    solved task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+// select login form
+const loginForm = document.querySelector('.login-form');
 
+// add submit listener
+loginForm.addEventListener('submit', onInputSubmit);
+
+// function that checks input and send login data to an object.
+function onInputSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (!email.value || !password.value) {
+    return alert('All fields must be filled in!!!');
+  }
+
+  const loginData = { email: email.value, password: password.value };
+
+  console.log(loginData);
+  event.currentTarget.reset();
+}
